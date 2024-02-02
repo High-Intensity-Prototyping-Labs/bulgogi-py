effdcddfc5f36ce2bcbce343f9cc26620ce18b4e
amellalalex <github@alexamellal.com>
2024-02-02 13:30:45 +0100

    Added the bul.init() method.

--- bulmodule.c
+++ bulmodule.c
@@ -1,6 +1,8 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+#include "bulgogi/inc/dot_bul.h"
+
 static PyObject *bul_system(PyObject *self, PyObject *args) {
         const char *command;
         int sts;
@@ -12,8 +14,16 @@ static PyObject *bul_system(PyObject *self, PyObject *args) {
         return PyLong_FromLong(sts);
 }
 
+static PyObject *bul_init(PyObject *self, PyObject *args) {
+        bul_dot_init();
+
+        Py_INCREF(Py_None);
+        return Py_None;
+}
+
 static PyMethodDef BulMethods[] = {
         {"system", bul_system, METH_VARARGS, "Execute a shell command."},
+        {"init", bul_init, METH_NOARGS, "Initializes the bulgogi project directory."},
         {NULL, NULL, 0, NULL},
 };
 
