4e9b45822a519b8a6b577f799d1111a306b4ecd1
amellalalex <github@alexamellal.com>
2024-02-02 13:46:52 +0100

    Added missing sources to setup.py - still missing libyaml.

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ setup(
     ext_modules=[
         Extension(
             name="bulgogi",
-            sources=["bulmodule.c", "bulgogi/src/dot_bul.c"],
+            sources=[
+                "bulmodule.c", 
+                "bulgogi/src/dot_bul.c", 
+                "bulgogi/src/engine.c",
+                "bulgogi/src/fs.c",
+                "bulgogi/src/yaml_ext.c",
+            ],
             include_dirs=["bulgogi/inc"],
         ),
     ]
