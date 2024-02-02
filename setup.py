aad83b2a91cdbe9f7c1e5003f218451028cb783c
amellalalex <github@alexamellal.com>
2024-02-02 23:06:26 +0100

    Modified setup() to run custom build command wrapper. Still needs work.
    Does not seem to work with build_ext or pip install -e .

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
 from setuptools import Extension, setup 
 
+from contextlib import suppress 
+from pathlib import Path 
+from setuptools import Command 
+from setuptools.command.build import build 
+
+class MakeCommand(Command):
+    def initialize_options(self) -> None:
+        self.bdist_dir = None 
+
+    def finalize_options(self) -> None:
+        with suppress(Exception):
+            self.bdist_dir = Path(self.get_finalized_command("bdist_wheel").bdist_dir)
+
+    def run(self) -> None:
+        if self.bdist_dir:
+            print("self.bdist_dir was successfully found!")
+
+class MakeBuild(build):
+    sub_commands = [('build_custom', None)] + build.sub_commands
+
 setup(
+    cmdclass={'build': MakeBuild, 'build_custom': MakeCommand},
     ext_modules=[
         Extension(
             name="bulgogi",
