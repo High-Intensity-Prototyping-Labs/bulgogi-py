0f594459fa9cc5055da76883261607bc314d1109
amellalalex <github@alexamellal.com>
2024-02-05 01:10:17 +0100

    Changed pyproject cibuildwheel conf to run gitinit.sh separately from build.sh.
    The build.sh script should be reserved for manual builds.

--- build.sh
+++ build.sh
@@ -10,13 +10,7 @@
 #       The necessary build deps will be fetched, compiled and later linked into a pip wheel.
 ################################
 
-git submodule init bulgogi 
-git submodule update bulgogi
-
-cd bulgogi 
-git submodule init libyaml 
-git submodule update libyaml
-cd ..
+source gitinit.sh
 
 python3 setup.py build_ext
 
