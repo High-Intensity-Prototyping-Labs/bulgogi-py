6f78520fd4e5ab5efac223d9414907ed8180d357
amellalalex <github@alexamellal.com>
2024-02-10 22:58:10 +0100

    Rudimentary bulmodule which loads core.
    Core is loaded as a dictionary of lists by target name.

--- bulmodule.c
+++ bulmodule.c
@@ -16,10 +16,16 @@ static PyObject *bul_py_system(PyObject *self, PyObject *args) {
 }
 
 static PyObject *bul_py_core_from_file(PyObject *self, PyObject *args) {
-        FILE *file = NULL;
         bul_core_s core;
-        const char *filename;
-        char *build_str = NULL;
+        size_t     x, y;
+        FILE       *file      = NULL;
+        const char *filename  = NULL;
+        char       *build_str = NULL;
+        PyObject   *dict      = NULL;
+        PyObject   *next_str  = NULL;
+        PyObject   *next_list = NULL;
+        bul_id_t   dep_id     = BUL_MAX_ID;
+
 
         if(!PyArg_ParseTuple(args, "s", &filename)) {
                 return NULL;
@@ -29,11 +35,30 @@ static PyObject *bul_py_core_from_file(PyObject *self, PyObject *args) {
                 return NULL;
         }
 
-        return NULL;
+        core = bul_core_from_file(file);
+
+        fclose(file);
+
+        dict = PyDict_New();
+        for(size_t x = 0; x < core.size; x++) {
+                next_list = PyList_New(core.targets[x].size);
+
+                for(size_t y = 0; y < core.targets[x].size; y++) {
+                        dep_id = core.targets[x].deps[y];
+                        next_str = PyUnicode_FromString(core.targets[dep_id].name);
+
+                        PyList_SetItem(next_list, y, next_str);
+                }
+
+                PyDict_SetItemString(dict, core.targets[x].name, next_list);
+        }
+
+        return dict;
 }
 
 static PyMethodDef BulMethods[] = {
         {"system", bul_py_system, METH_VARARGS, "Execute a shell command."},
+        {"core_from_file", bul_py_core_from_file, METH_VARARGS, "Initializes a core from a YAML file."},
         {NULL, NULL, 0, NULL},
 };
 
