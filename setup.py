3bc968c84efae18c6126bbf0e332d22c1e865990
amellalalex <github@alexamellal.com>
2024-02-11 02:41:47 +0100

    Adapted to new cibuildwheel Makefile entry.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools.command.build_ext import build_ext
 from os import system
 
 def Make():
-    system('make -C bulgogi libs')
+    system('make -C bulgogi cibuildwheel')
 
 class MakeBuildExt(build_ext):
     def run(self) -> None:
